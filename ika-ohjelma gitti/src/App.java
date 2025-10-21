f4c85ef8f90c660018a495df99e4bf21348734f5
Juhani Rannanjärvi <amk1013363@student.hamk.fi>
2025-10-21 22:10:51 +0300

    Perusehdot
    Perusehdot on annettu, lisätty alaikäiset ja aikuiset ja int ikä

--- ika-ohjelma gitti/src/App.java
+++ ika-ohjelma gitti/src/App.java
@@ -1,6 +1,26 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int ika = 18;
+
+       // Perusehdot
+        if (ika >= 0 && ika < 18) {
+            System.out.println("Olet alaikäinen");
+            if (ika >= 15) {
+                System.out.println("Saat ajaa mopoa");
+            }
+            if (ika >= 16) {
+                System.out.println("Saat ajaa kevaria");
+            }
+
+        } else if (ika >= 18 && ika < 65) {
+            System.out.println("Olet aikuinen");
+            if (ika == 18) {
+                System.out.println("Olet täysiikäinen ja voit ajaa autoa");
+            }
+        }    
     }
-    asdasda s asaas
-}
+}    
+           
+            
+           
+            
